ebede57d63e4ca7a53ad77d9c5c01c1f73c9aa3b
Maksym Shymanskyi <makskampod@gmail.com>
2023-06-12 16:29:21 +0300

    Added login form

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
   return (
-    <div>Login</div>
+  <div className="login">
+    <h1>Login</h1>
+    <form action="POST">
+      <input value={email} type="email" id="email" placeholder='Email' onChange={(e) => {setEmail(e.target.value)}}/>
+      <input value={password} type="password" id="password" placeholder='Password' onChange={(e) => {setPassword(e.target.value)}}/>
+      <input type="submit" />
+    </form>
+    <div>
+      OR <Link to="/signup">Signup Page</Link>
+    </div>
+  </div>
   )
 }
 
